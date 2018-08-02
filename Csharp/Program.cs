4255c3b978533beb24aee9c3c04e9fe6e03cb3bd
halyhuang1991 <18807898837@139.com>
2018-08-02 10:06:10 +0800

    1

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -11,7 +11,7 @@ namespace Csharp
         {
         
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //Console.ReadKey();
         }
         private static void redisGo(){
              redis.SetString("key", "value123");
