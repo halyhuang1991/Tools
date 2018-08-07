c3accc1c6d4fde87b2f483849457fd301ae5c33c
halyhuang1991 <18807898837@139.com>
2018-08-07 11:21:34 +0800

    1

--- js/arr.js
+++ js/arr.js
@@ -61,3 +61,53 @@ ww.instance().b('ok');
 var rr=ww.instance().c;
 rr.add(233);
 console.log(rr.arr)
+//----------------------
+var getSingle = function(fn){ //参数为创建对象的方法
+    var result;
+    return function(){ //判断是Null或赋值
+        return result || (result = fn.apply(this,arguments));
+    };
+};
+//--------------------
+var mult = function () {
+    var a = 1;
+    for (var i = 0, l = arguments.length; i < l; i++) {
+        a = a * arguments[i];
+    }
+    return a;
+}
+//不使用缓存mult(2,3);
+var proxyMult = (function () {
+    var cache = {};
+    return function () {
+        var args = Array.prototype.join.call(arguments, ','); //把参数放在一个字符串里
+        if (args in cache) {
+            return cache[args];
+        }
+        return cache[args] = mult.apply(this,arguments);
+    };
+})();
+//使用缓存proxyMult(2,3)
+//------------------------------状态模式
+var Light = function () {
+    this.state = "off";
+    this.button = null;
+};
+var LightEvent = {
+    on:function(){
+        console.log("请关灯");
+        this.state = "off";
+    },
+    off:function(){
+        console.log("请开灯");
+        this.state = "on";
+    }
+};
+Light.prototype.press=function(){
+    LightEvent[this.state].apply(this,arguments);
+    console.log(this.state)
+}
+var l=new Light("on",null);
+l.press();
+l.press();
+
