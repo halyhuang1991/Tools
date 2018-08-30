55a29eae998ede6c91362bb7abbac110fdc1ad48
halyhuang1991 <18807898837@139.com>
2018-08-30 09:57:38 +0800

    1

--- ts/shapeConsumer.js
+++ ts/shapeConsumer.js
@@ -3,3 +3,6 @@ exports.__esModule = true;
 var shapes = require("./shapes");
 var t = new shapes.Triangle("ok");
 t.log();
+// $.each(['a','b','c'], function (index, value) {
+//     console.log(index + " " + value);
+// })
--- ts/shapeConsumer.ts
+++ ts/shapeConsumer.ts
@@ -1,3 +1,6 @@
 import * as shapes from "./shapes";
 var t = new shapes.Triangle("ok");
 t.log();
+// $.each(['a','b','c'], function (index, value) {
+//     console.log(index + " " + value);
+// })
