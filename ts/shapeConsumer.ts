2710151d596f3a99f6b25473ddf8b1ac724883d0
halyhuang1991 <18807898837@139.com>
2018-08-31 16:03:50 +0800

    1

--- ts/shapeConsumer.ts
+++ ts/shapeConsumer.ts
@@ -1,6 +1,3 @@
 import * as shapes from "./shapes";
 var t = new shapes.Triangle("ok");
 t.log();
-// $.each(['a','b','c'], function (index, value) {
-//     console.log(index + " " + value);
-// })
