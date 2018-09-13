06daf6c3ebd400159e9fa5bf2e7d25bda7963fd9
halyhuang1991 <18807898837@139.com>
2018-09-13 08:47:03 +0800

    1

---
+++ Csharp/Models/SetG.cs
@@ -0,0 +1,15 @@
+namespace Csharp.Models
+{
+    public class SetG
+    {
+        private string _str="s";
+        public string str{
+            get{
+                return _str;
+            }
+            set{
+                _str=value;
+            }
+        }
+    }
+}
--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -9,7 +9,7 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\r\nHello World!");
             //Console.ReadKey();
         }
        
