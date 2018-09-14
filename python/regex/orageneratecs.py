00bbb28b38ceb254847d92743bf2b2335fe12d9d
halyhuang1991 <18807898837@139.com>
2018-09-14 09:07:28 +0800

    1

---
+++ python/regex/orageneratecs.py
@@ -0,0 +1,44 @@
+import re
+import os
+def is_contains(n):
+    return n.find("=")>0
+path=r"D:\C\github\Tools\python\regex\produre.txt"
+f = open(path, 'r',encoding='UTF-8')
+text=f.read()
+arr=re.findall(r'[^()]+',text)
+arr=list(set(arr))
+arr=list(filter(is_contains,arr))
+print(len(arr))
+arr=arr[0].split(',')
+pms=''
+args=''
+i=0
+for str1 in arr:
+    key=str1.split('=>')[0].strip()
+    if str1.find(':')>0:
+        args+=('' if args=='' else ',')+'string '+key
+        len1=""
+        if key in ["outfname","err_msg"]:
+            len1="Varchar2,500"
+        elif key.find("dte")>0:
+            len1="Date"
+        else:
+            len1="Varchar2"
+           
+        pms+='cmdParas[{0}] = new OracleParameter("{1}", OracleDbType.{2});\n'.format(i,key,len1)
+       
+        if len1.find('500')>0:
+            pms+='cmdParas[{0}].Direction = ParameterDirection.Output;\n'.format(i)
+        else:
+            pms+='cmdParas[{0}].Value = {1};\n'.format(i,key)
+    else:
+        args+=('' if args=='' else ',')+'string[] '+key
+        pms+='cmdParas[{0}] = new OracleParameter("{1}", OracleDbType.Varchar2);\n'.format(i,key)
+        pms+='cmdParas[{0}].CollectionType = OracleCollectionType.PLSQLAssociativeArray;\n'.format(i)
+        pms+='cmdParas[{0}].Value = {1};\n'.format(i,key)
+    i=i+1
+pms='OracleParameter[] cmdParas = new OracleParameter[{0}];\n'.format(i)+pms
+print(pms)
+print(args)
+        
+        
