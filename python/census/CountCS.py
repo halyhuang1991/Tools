504304ed536fe31943392d68228337f389f07613
halyhuang1991 <18807898837@139.com>
2018-11-08 15:27:33 +0800

    1

--- python/census/CountCS.py
+++ python/census/CountCS.py
@@ -6,15 +6,22 @@ def GetLines(path):
     length = len(open(path, 'r',-1,'UTF-8').readlines())
     return length
 
+def GetCodeCount(path):
+	count = 0
+	codelinescount=0
+	for root,dirs,files in os.walk(path):
+		for each in files:
+			if each.find(".cs")>0:
+				count+=1
+				#print(root+"\\"+each)
+			else:
+				#print(root+"\\"+each)
+				filepath=root+"\\"+each
+				codelinescount+=GetLines(filepath)
+	return {"pageCount":count,"CodeLines":codelinescount}
 
-count = 0
-for root,dirs,files in os.walk(path):
-	for each in files:
-		if each.find(".cs")>0:
-			count+=1
-			#print(root+"\\"+each)
-		else:
-			#print(root+"\\"+each)
-			filepath=root+"\\"+each
-			print(GetLines(filepath),each)
-print(count)
+print(GetCodeCount(path))
+
+
+
+
