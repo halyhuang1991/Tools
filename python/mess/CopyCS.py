a1cdf3d16d35262ccd5f67a7fa38abec42c4a038
halyhuang1991 <18807898837@139.com>
2018-11-26 17:50:12 +0800

    1

--- python/mess/CopyCS.py
+++ python/mess/CopyCS.py
@@ -5,10 +5,10 @@ def custombasename(path):
     return os.path.basename(os.path.splitext(path)[0])
 def Getdir(path):
     return os.path.dirname(path)
-def CopyRewrite(path,newname,dic):
+def CopyRewrite(path,newname,dic,modelname):
     filename=os.path.basename(path)
     print(filename)
-    clsname=custombasename(path).replace('VAT',newname)
+    clsname=custombasename(path).replace(modelname,newname)
     print(clsname)
     newpath=Getdir(path)+'\\'+clsname+GetFileExtension(path)
     print(newpath)
@@ -18,8 +18,10 @@ def CopyRewrite(path,newname,dic):
         return
     f_new = open(newpath,'w',encoding='utf-8')
     f= open(path,'r',-1,'utf-8')
-    for line in f:
+    print(len(f.readlines()))
+    for line in f.readlines():
         line=line.replace(custombasename(path).split('.')[0],clsname)
+        print(line)
         for key in dic:
             line=line.replace(key,dic[key])
         f_new.write(line)
@@ -28,7 +30,7 @@ def CopyRewrite(path,newname,dic):
     #os.rename(newpath,newpath.replace('3','34'))
 def WriteNew():
     path=r"D:\NewWork\BEWeb\BENet\BE.Web\Handlers\Basdat\VATDataHandler.ashx"
-    CopyRewrite(path,'Price',{})
+    CopyRewrite(path,'Price',{},'VAT')
     dic={}
     dic['ORFVATA']='ORFVATA'
     dic['VAC,VACD,VATTP']=''
@@ -37,9 +39,24 @@ def WriteNew():
     dic['VACD']='VACD'
     dic['VATTP']='VATTP'
     path=r"D:\NewWork\BEWeb\BENet\BE.Web\App_Code\Handlers\Basdat\VATDataHandler.ashx.cs"
-    CopyRewrite(path,'Price',dic)
+    CopyRewrite(path,'Price',dic,'VAT')
     print('ok')
-WriteNew()
+def WriteNew1(name):
+    if name=='':
+        return
+    path=r"D:\NewWork\BEWeb\BENet\BE.Web\Handlers\Salord\SpecialOrderDataHandler.ashx"
+    CopyRewrite(path,name,{},'SpecialOrder')
+    dic={}
+    dic['IMFCLTC']='IMFCLTC'
+    dic['CLSPTP,CLSPTD,FLAG2']=''
+    dic['Special Order,Description,Printing Flag for Bogart Report']=''
+    dic['CLSPTP']='CLSPTP'
+    dic['CLSPTD']='CLSPTD'
+    dic['VATTP']='FLAG2'
+    path=r"D:\NewWork\BEWeb\BENet\BE.Web\App_Code\Handlers\Salord\SpecialOrderDataHandler.ashx.cs"
+    CopyRewrite(path,name,dic,'SpecialOrder')
+    print('ok')
+WriteNew1('CMLabDip')
 
 
 
